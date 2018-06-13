d616a50377e885aa0778604541e290e3ffc98c8a
work <work@Binus-MacBook-Pro-2.local>
2018-06-13 18:05:18 +0100

    adding treemap

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './assets/styles/normalize.v8.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App/App';
+
+import data from './data.json';
+
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
